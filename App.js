8bd5002a1e68a81c803806a5e0b018a0b2ff81aa
PauloBezz <gustavosilvah37@gmail.com>
2024-04-29 21:02:45 -0300

    feat: project modfied

--- App.js
+++ App.js
@@ -1,74 +1,57 @@
-import React from "react";
-import {
-  StyleSheet,
-  Button,
-  View,
-  SafeAreaView,
-  Text,
-  Alert,
-} from "react-native";
+import { View, Text } from "react-native";
 
-const Separator = () => <View style={styles.separator} />;
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "blue",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 50,
+          fontWeight: "bold",
+          color: "white",
+        }}
+      >
+        INDMO
+      </Text>
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <View>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert("Simple Button pressed")}
-      />
+      <Text
+        style={{
+          paddingTop: 20,
+          fontSize: 30,
+          color: "yellow",
+        }}
+      >
+        HELLO WOLRD!
+      </Text>
     </View>
-    <Separator />
-    <View>
-      <Button
-        title="Press me"
-        color="#f194ff"
-        onPress={() => Alert.alert("Button with adjusted color pressed")}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Button
-        title="Press me"
-        color="green"
-        onPress={() => Alert.alert("Cannot press this one")}
-      />
-    </View>
-    <Separator />
-    <View>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          onPress={() => Alert.alert("Left button pressed")}
-        />
-        <Button
-          title="Right button"
-          onPress={() => Alert.alert("Right button pressed")}
-        />
-      </View>
-    </View>
-  </SafeAreaView>
-);
+  );
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    marginHorizontal: 16,
-  },
-  title: {
-    textAlign: "center",
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: "#737373",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
-
-export default App;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     textAlign: "center",
+//     marginVertical: 8,
+//   },
+//   fixToText: {
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//   },
+//   separator: {
+//     marginVertical: 8,
+//     borderBottomColor: "#FFFFF",
+//     borderBottomWidth: StyleSheet.hairlineWidth,
+//   },
+// });
