e4b3111ae151bbc3636e80d2c7c930fdc65b68db
PauloBezz <gustavosilvah37@gmail.com>
2024-05-21 22:09:27 -0300

    feat: Weather detail view created

--- src/type-weather/pages/dashboard/index.jsx
+++ src/type-weather/pages/dashboard/index.jsx
@@ -1,4 +1,6 @@
 import {
+  ScrollScreen,
+
   Container,
   TopContainer,
   TopContainerImage,
@@ -11,23 +13,42 @@ import {
   TopContainerHeaderIn,
   TopContainerFooterIn,
   TopContainerFooterImage,
-  TopContainerFooterTitle,
   TopContainerFooterInLeft,
   TopContainerFooterInRight,
   TopContainerFooterInText,
   TopContainerFooterInTitle,
-  TopContainerFooterSubtitle,
+  TopContainerFooterInSubtitle,
+
+  MiddleContainer,
+  MiddleContainerCard,
+  MiddleContainerImage,
+  MiddleContainerText,
+  MiddleContainerTermometerImageSize,
+  MiddleContainerRainyCloudImageSize,
+  MiddleContainerUmitImageSize,
+  MiddleContainerWindImageSize,
+  MiddleContainerSunImageSize,
+  MiddleContainerCardLine
 } from "./styles";
 
-import Cloud from "./images/home.png";
 import Background from "../../assets/images/background.png";
-import TopContainerBackground from "./images/background-dashboard.png";
+
+import Cloud from "./images/home.png";
 import Icon from "./images/icons.png";
+import TopContainerBackground from "./images/background-dashboard.png";
+import Termometer from "./images/termometer.png";
+import RainyCloud from "./images/rainy-cloud.png";
+import Wind from "./images/wind.png";
+import Umit from "./images/umit.png";
+import Sun from "./images/sun.png";
 
 export default function Dashboard() {
   return (
+
+    
     //All Content
     <Container source={Background}>
+      <ScrollScreen>
       {/* Container do Topo */}
       <TopContainer>
         {/* Header do Topo */}
@@ -40,6 +61,8 @@ export default function Dashboard() {
             placeholderTextColor="#7F7F98"
           />
         </TopContainerHeader>
+
+        
         {/* Imagem do Container do Topo*/}
         <TopContainerBackgroundImage source={TopContainerBackground}>
           {/* Header da Imagem do Container do Topo*/}
@@ -53,10 +76,10 @@ export default function Dashboard() {
 
           <TopContainerFooterIn>
             <TopContainerFooterInLeft>
-              <TopContainerFooterTitle>28°c</TopContainerFooterTitle>
-              <TopContainerFooterSubtitle>
+              <TopContainerFooterInTitle>28°c</TopContainerFooterInTitle>
+              <TopContainerFooterInSubtitle>
                 26ºc / 32ºc
-              </TopContainerFooterSubtitle>
+              </TopContainerFooterInSubtitle>
               <TopContainerFooterInText>Poucas nuvens</TopContainerFooterInText>
             </TopContainerFooterInLeft>
 
@@ -66,6 +89,68 @@ export default function Dashboard() {
           </TopContainerFooterIn>
         </TopContainerBackgroundImage>
       </TopContainer>
+
+      <MiddleContainer>
+        <MiddleContainerCard>
+          <MiddleContainerImage>
+            <MiddleContainerTermometerImageSize source={Termometer} />
+            <MiddleContainerText>Sensação térmica</MiddleContainerText>
+          </MiddleContainerImage>
+          <MiddleContainerImage>
+            <MiddleContainerText>26ºc</MiddleContainerText>
+          </MiddleContainerImage>
+        </MiddleContainerCard>
+
+        <MiddleContainerCardLine/>
+
+        <MiddleContainerCard>
+          <MiddleContainerImage>
+            <MiddleContainerRainyCloudImageSize source={RainyCloud} />
+            <MiddleContainerText>Probabilidade de chuva</MiddleContainerText>
+          </MiddleContainerImage>
+          <MiddleContainerImage>
+            <MiddleContainerText>0%</MiddleContainerText>
+          </MiddleContainerImage>
+        </MiddleContainerCard>
+
+        <MiddleContainerCardLine/>
+
+        <MiddleContainerCard>
+          <MiddleContainerImage>
+            <MiddleContainerWindImageSize source={Wind} />
+            <MiddleContainerText>Velocidade do vento</MiddleContainerText>
+          </MiddleContainerImage>
+          <MiddleContainerImage>
+            <MiddleContainerText>8 km/h</MiddleContainerText>
+          </MiddleContainerImage>
+        </MiddleContainerCard>
+
+        <MiddleContainerCardLine/>
+
+        <MiddleContainerCard>
+          <MiddleContainerImage>
+            <MiddleContainerUmitImageSize source={Umit} />
+            <MiddleContainerText>Umidade do ar</MiddleContainerText>
+          </MiddleContainerImage>
+          <MiddleContainerImage>
+            <MiddleContainerText>40%</MiddleContainerText>
+          </MiddleContainerImage>
+        </MiddleContainerCard>
+
+        <MiddleContainerCardLine/>
+
+        <MiddleContainerCard>
+          <MiddleContainerImage>
+            <MiddleContainerSunImageSize source={Sun} />
+            <MiddleContainerText>Índice UV</MiddleContainerText>
+          </MiddleContainerImage>
+          <MiddleContainerImage>
+            <MiddleContainerText>5</MiddleContainerText>
+          </MiddleContainerImage>
+        </MiddleContainerCard>
+      </MiddleContainer>
+      </ScrollScreen>
     </Container>
+   
   );
 }
--- src/type-weather/pages/dashboard/styles.js
+++ src/type-weather/pages/dashboard/styles.js
@@ -4,6 +4,8 @@ const Container = styled.ImageBackground`
   flex: 1;
   display: flex;
   flex-direction: column;
+  width: 100%;
+  height: 100%;
 `;
 
 const TopContainer = styled.View`
@@ -93,13 +95,13 @@ const TopContainerFooterInLeft = styled.View`
   flex-direction: column;
 `;
 
-const TopContainerFooterTitle = styled.Text`
+const TopContainerFooterInTitle = styled.Text`
   color: #fafafa;
   font-size: 48px;
   font-family: "Nunito-ExtraBold";
 `;
 
-const TopContainerFooterSubtitle = styled.Text`
+const TopContainerFooterInSubtitle = styled.Text`
   color: #fafafa;
   font-size: 16px;
   font-family: "Nunito-Bold";
@@ -121,7 +123,77 @@ const TopContainerFooterImage = styled.Image`
   height: 160px;
 `;
 
+const MiddleContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  gap: 10px;
+  margin-top: 14px;
+  margin-left: 12px;
+  margin-right: 12px;
+  background-color: #16161f;
+  border-radius: 12px;
+`;
+
+const MiddleContainerCard = styled.View`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  width: 100%;
+`;
+
+const MiddleContainerCardLine = styled.View`
+  border: 0.4px solid #3B3B54;
+  width: 90%;
+  opacity: 0.5;
+  margin-top: -15px;
+  margin-bottom: -15px;
+`;
+
+const MiddleContainerImage = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding: 22.19px 20.94px;
+  justify-content: space-between;
+`;
+
+const MiddleContainerTermometerImageSize = styled.Image`
+  width: 11.63px;
+  height: 22.12px;
+`;
+
+const MiddleContainerRainyCloudImageSize = styled.Image`
+  width: 19.1px;
+  height: 21.38px;
+`;
+
+const MiddleContainerWindImageSize = styled.Image`
+  width: 20.63px;
+  height: 16.13px;
+`;
+
+const MiddleContainerUmitImageSize = styled.Image`
+  width: 16.13px;
+  height: 20.63px;
+`;
+
+const MiddleContainerSunImageSize = styled.Image`
+  width: 19.13px;
+  height: 19.13px;
+`;
+
+const MiddleContainerText = styled.Text`
+  color: #fafafa;
+  font-size: 14px;
+  font-family: "Nunito-Bold";
+  padding: 0px 0px 0px 12px;
+`;
+
+const ScrollScreen = styled.ScrollView``;
+
 export {
+  ScrollScreen,
+
   Container,
   TopContainer,
   TopContainerImage,
@@ -134,9 +206,20 @@ export {
   TopContainerHeaderIn,
   TopContainerFooterIn,
   TopContainerFooterImage,
-  TopContainerFooterTitle,
+  TopContainerFooterInTitle,
   TopContainerFooterInLeft,
   TopContainerFooterInRight,
   TopContainerFooterInText,
-  TopContainerFooterSubtitle,
+  TopContainerFooterInSubtitle,
+
+  MiddleContainer,
+  MiddleContainerCard,
+  MiddleContainerImage,
+  MiddleContainerText,
+  MiddleContainerTermometerImageSize,
+  MiddleContainerRainyCloudImageSize,
+  MiddleContainerUmitImageSize,
+  MiddleContainerWindImageSize,
+  MiddleContainerSunImageSize,
+  MiddleContainerCardLine
 };
