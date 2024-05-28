93ee93c11552f9eb1d9b88ef18bf105101b89d51
PauloBezz <gustavosilvah37@gmail.com>
2024-05-27 22:26:18 -0300

    feat: OpenWeather API consumed

--- src/type-weather/pages/dashboard/index.jsx
+++ src/type-weather/pages/dashboard/index.jsx
@@ -51,9 +51,55 @@ import Sun from "./images/sun.png";
 
 import StormyCloudIcon from "./images/stormy-cloud-icon.png"
 import SunnyIcon from "./images/sunny-icon.png"
+import { useState } from "react";
+import axios from "axios";
+
 
 
 export default function Dashboard() {
+
+  const [stateTemperatura, setStateTemperatura] = useState("");
+  const [stateCity, setStateCity] = useState("");
+  const [city, setCity] = useState("");
+  const [stateTempMax, setStateTempMax] = useState("");
+  const [stateTempMin, setStateTempMin] = useState("");
+
+  const [stateChanceOfRain, setStateChanceOfRain] = useState();
+  const [stateFeelsLike, setStateFeelsLike] = useState();
+  const [stateHumidity, setStateHumidity] = useState();
+  const [stateUvIndex, setStateUvIndex] = useState();
+  const [stateWindKm, setStateWindKm] = useState();
+
+  const key = "b2213bc688da17e335540298fee4f0b7";
+  const detailKey = "f325bd9a5efd4399858165721242405";
+
+  const getClima = () => {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${stateCity}&lang=pt_br&appid=${key}&units=metric&`;
+    const weatherDetailsUrl = `http://api.weatherapi.com/v1/forecast.json?key=${detailKey}&q=${stateCity}`
+
+    axios.get(weatherUrl)
+      .then((weatherData) => {
+        setStateTemperatura(weatherData.data.main.temp)
+        setStateTempMax(weatherData.data.main.temp_max)
+        setStateTempMin(weatherData.data.main.temp_min)
+      })
+
+      axios.get(weatherDetailsUrl)
+      .then((detailInfo) => {
+        console.log(detailInfo.data.current)
+
+        setStateFeelsLike(detailInfo.data.current.feelslike_c);
+        setStateChanceOfRain(detailInfo.data.forecast.
+          forecastday[0].day.daily_chance_of_rain);
+        setStateHumidity(detailInfo.data.current.humidity);
+        setStateUvIndex(detailInfo.data.current.uv);
+        setStateWindKm(detailInfo.data.current.wind_kph);
+      })
+
+      setStateCity('')
+  }
+
+
   return (
 
     
@@ -70,6 +116,12 @@ export default function Dashboard() {
           <TopContainerInput
             placeholder="Buscar local"
             placeholderTextColor="#7F7F98"
+            value={stateCity}
+            onSubmitEditing={getClima}
+            onChangeText={ newText => {
+              setStateCity(newText)
+              setCity(newText)
+            }}
           />
         </TopContainerHeader>
 
@@ -78,7 +130,7 @@ export default function Dashboard() {
         <TopContainerBackgroundImage source={TopContainerBackground}>
           {/* Header da Imagem do Container do Topo*/}
           <TopContainerHeaderIn>
-            <TopContainerTitle>Porto Alegre, RS</TopContainerTitle>
+            <TopContainerTitle>{city}</TopContainerTitle>
             <TopContainerHourText>21:39</TopContainerHourText>
           </TopContainerHeaderIn>
           <TopContainerSubtitle>
@@ -87,9 +139,9 @@ export default function Dashboard() {
 
           <TopContainerFooterIn>
             <TopContainerFooterInLeft>
-              <TopContainerFooterInTitle>28°c</TopContainerFooterInTitle>
+              <TopContainerFooterInTitle>{!Number.isNaN(parseInt(stateTemperatura)) ? `${parseInt(stateTemperatura)}°c` : " - "}</TopContainerFooterInTitle>
               <TopContainerFooterInSubtitle>
-                26ºc / 32ºc
+              {!Number.isNaN(parseInt(stateTempMax)) && !Number.isNaN(parseInt(stateTempMin)) ? `${parseInt(stateTempMax)}°c / ${parseInt(stateTempMin)}°c` : " - / -"}
               </TopContainerFooterInSubtitle>
               <TopContainerFooterInText>Poucas nuvens</TopContainerFooterInText>
             </TopContainerFooterInLeft>
@@ -108,7 +160,7 @@ export default function Dashboard() {
             <MiddleContainerText>Sensação térmica</MiddleContainerText>
           </MiddleContainerImage>
           <MiddleContainerImage>
-            <MiddleContainerText>26ºc</MiddleContainerText>
+            <MiddleContainerText>{!Number.isNaN(parseInt(stateFeelsLike)) ? `${parseInt(stateFeelsLike)}°c` : " - "}</MiddleContainerText>
           </MiddleContainerImage>
         </MiddleContainerCard>
 
